9d64db17d69fc58bcd93127f998c4bebda4b27de
riya200-ui <panchalriya1976@gmail.com>
2022-05-24 14:24:09 +0530

    deploy crud

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
